83bacb11b313d4d4aa6a0776ccc3a12757bab160
Aditya Raj <adityakarn06@gmail.com>
2025-04-02 21:11:01 +0530

    added functionaly to view menu button

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
       <div className="header-contents">
         <h2><span>Discover</span> the best food & drinks</h2>
         <p>Explore curated lists of top restaurants, cafes, pubs, and bars in your city, based on trends</p>
-        <button>View menu</button>
+        <a href="#explore-menu"><button>View menu</button></a>
       </div>
       <div className="hero-img">
         <img src={assets.header_img} alt="" />
